278ecf5768e91905b6644a0be6a2d7dbc318dc66
Paolo D'Incau <paolo.dincau@xpeppers.com>
2015-11-03 20:55:00 +0100

    Adding mock to test

--- test/api_test.exs
+++ test/api_test.exs
@@ -7,6 +7,7 @@ defmodule ApiTest do
   @username Application.get_env(:api, :basic_auth_user)
   @password Application.get_env(:api, :basic_auth_password)
 
+  import Mock
 
   test "/users with incorrect credentials returns 401" do
     conn = conn(:get, "/users")
@@ -18,12 +19,18 @@ defmodule ApiTest do
   end
 
   test "/users with correct credentials returns 200" do
-    conn = conn(:get, "/users")
-    |> put_req_header("authorization", auth_headers_for(@username, @password))
-    |> Router.call([])
+    users = [%User{name: "Paolo", surname: "D'Incau"}]
+    users_as_json = Poison.encode!(users)
 
-    assert conn.state == :sent
-    assert conn.status == 200
+    with_mock App, [users: fn ->  users end] do
+      conn = conn(:get, "/users")
+      |> put_req_header("authorization", auth_headers_for(@username, @password))
+      |> Router.call([])
+
+      assert conn.state == :sent
+      assert conn.status == 200
+      assert conn.resp_body == users_as_json
+    end
   end
 
   defp auth_headers_for(username, password) do
