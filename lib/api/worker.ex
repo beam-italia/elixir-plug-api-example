9d30174fc4dbad23df1e8964334cd253df2e7d71
Matteo <pierromatteo@gmail.com>
2015-11-05 18:16:32 +0100

    removing space

--- lib/api/worker.ex
+++ lib/api/worker.ex
@@ -1,7 +1,7 @@
 defmodule Api.Worker do
 
   def start_link do
-    Plug.Adapters.Cowboy.http(Api.Router, [],  port: Application.get_env(:api, :port))
+    Plug.Adapters.Cowboy.http(Api.Router, [], port: Application.get_env(:api, :port))
   end
 
 end
