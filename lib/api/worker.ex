220345ad64f0e33e925743a754fd3ef04745fd22
Matteo <pierromatteo@gmail.com>
2015-11-05 19:32:13 +0100

    extract http_port var in api worker

--- lib/api/worker.ex
+++ lib/api/worker.ex
@@ -1,7 +1,9 @@
 defmodule Api.Worker do
 
+  @http_port Application.get_env(:api, :http_port)
+
   def start_link do
-    Plug.Adapters.Cowboy.http(Api.Router, [], port: Application.get_env(:api, :port))
+    Plug.Adapters.Cowboy.http(Api.Router, [], port: @http_port)
   end
 
 end
